9c33623ced23d6971a50ed38ee0bd12b2de78ea0
Climber <jo.climber10@gmail.com>
2023-03-30 11:07:08 +0200

    Completed #9 with basic auth

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const basicAuth = require('express-basic-auth')
 const cors = require("cors");
 const fs = require("fs");
 var queue = require('express-queue');
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(queue({ activeLimit: 5, queuedLimit: -1 }));
+app.use(basicAuth({users: { 'admin': 'admin1234' }}))
 
 const routes = fs.readdirSync('./routes').filter(route => route.endsWith('.js'));
 
@@ -26,3 +28,4 @@ routes.forEach((path) => {
 app.listen(port, () => {
 	console.log(`Server is running on port: ${port}`);
 });
+
